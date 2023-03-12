9c1f3c5d34284c3d281ea97a485780f4ee04409d
Vladislav Smirnov <beluckydaf@gmail.com>
2023-03-13 00:22:14 +0300

    Add tabstop

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	'f-person/git-blame.nvim',
+	config = function ()
+		vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+		local git_blame = require('gitblame')
+
+		require('lualine').setup({
+			sections = {
+				lualine_c = {
+					{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
+				}
+			}
+		})
+	end
+}
